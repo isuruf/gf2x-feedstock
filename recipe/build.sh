328947237572a59718ee536a2c8da16807e82a50
Isuru Fernando <isuruf@gmail.com>
2018-09-11 18:34:56 -0500

    Add windows

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,28 @@
 chmod +x configure
 
 export CFLAGS="-O2 -g $CFLAGS"
-./configure --prefix="$PREFIX" --libdir="$PREFIX"/lib --disable-hardware-specific-code
+
+
+case `uname` in
+    Darwin)
+    Linux)
+        ./configure --prefix="$PREFIX" --libdir="$PREFIX"/lib --disable-hardware-specific-code
+        ;;
+    MINGW*)
+        export PATH="$PREFIX/Library/bin:$BUILD_PREFIX/Library/bin:$PATH"
+        export CC=clang
+        export RANLIB=llvm-ranlib
+        export AS=llvm-as
+        export AR=llvm-ar
+        export CFLAGS="-MD -I$PREFIX/Library/include"
+        export LDFLAGS="$LDFLAGS -L$PREFIX/Library/lib"
+        clang --version
+        llvm-as --version
+        llvm-ar --version
+        ./configure --prefix="$PREFIX/Library" --libdir="$PREFIX/Library/lib" --disable-hardware-specific-code
+        ;;
+esac
+
 
 make
 make check
