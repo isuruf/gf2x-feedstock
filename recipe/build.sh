b11245a422d744a6d8b14795fe0540aca869287e
Isuru Fernando <isuruf@gmail.com>
2018-09-11 22:56:57 -0500

    Disable shared for now

--- recipe/build.sh
+++ recipe/build.sh
@@ -20,7 +20,7 @@ case `uname` in
         clang --version
         llvm-as --version
         llvm-ar --version
-        ./configure --prefix="$PREFIX/Library" --libdir="$PREFIX/Library/lib" --disable-hardware-specific-code
+        ./configure --prefix="$PREFIX/Library" --libdir="$PREFIX/Library/lib" --disable-hardware-specific-code --disable-shared
         ;;
 esac
 
